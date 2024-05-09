8d0fe4ea173bca485fd2fda6385b10a76b5d52a5
darialiit <solomkina.daria@gmail.com>
2024-05-09 16:05:18 -0300

    fix cart image

--- src/components/CartCount.tsx
+++ src/components/CartCount.tsx
@@ -1,11 +1,12 @@
 import { useMenuCartContext } from '../store/cartContext';
+import cartImg from '../assets/cart.png';
 
 export default function CartCount() {
   const { totalQuantity } = useMenuCartContext();
 
   return (
     <div id="menu-count">
-      <img src="../src/assets/cart.png" alt="cart" />
+      <img src={cartImg} alt="cart" />
       {totalQuantity === 0 ? '' : <div id="counter">{totalQuantity}</div>}
     </div>
   );
